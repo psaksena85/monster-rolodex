6d99eb87f8e03cef6f830b1d0e347bfe56e24633
Pulkit Saxena <saxenap@aetna.com>
2020-03-19 23:18:34 -0400

    Updates

---
+++ precache-manifest.ded8beac7fa04e847cd5ed72a3aabf5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "887e3055e07af862f8a0e088c6499edd",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "53ada6661222e2ee710d",
+    "url": "/monster-rolodex/static/css/main.cd5a596f.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monster-rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monster-rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53ada6661222e2ee710d",
+    "url": "/monster-rolodex/static/js/main.d7863928.chunk.js"
+  },
+  {
+    "revision": "bc09d86fba44e1c7ba83",
+    "url": "/monster-rolodex/static/js/runtime-main.8000f3b5.js"
+  }
+]);
